45f76165da3193a7125b9607554b8ad300b66597
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-10-02 18:12:22 +0700

    fix css

--- review.php
+++ review.php
@@ -279,9 +279,87 @@
       </div>
     </div>
     <!-- END SUCCESS STORY  -->
+
+    <!-- START PARENTS BLOCK -->
+    <div class="parents-block">
+      <div class="container">
+        <div class="parents-content-wrapper">
+          <div class="review-title-block">
+            <img src="./images/parent-title.png" class="review-title-image title-3 mr-b-12" />
+            <h2 class="review-title common-primary-title ">Khẳng Định Con Tiến Bộ Kỹ Năng Giao Tiếp <br /> Nhờ Sử Dụng
+              Bitu</h2>
+          </div>
+
+          <div class="parents-feed-back-list-wrapper">
+            <div class="parents-feed-back-list">
+              <div class="parents-feed-back-item">1</div>
+              <div class="parents-feed-back-item">2</div>
+              <div class="parents-feed-back-item">3</div>
+              <div class="parents-feed-back-item">4</div>
+              <div class="parents-feed-back-item">5</div>
+              <div class="parents-feed-back-item">6</div>
+              <div class="parents-feed-back-item">7</div>
+              <div class="parents-feed-back-item">8</div>
+            </div>
+            <button class="btn-slider btn-next">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <g clip-path="url(#clip0_5934_5832)">
+                  <path d="M9 6L15 12L9 18" stroke="#1A202C" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_5934_5832">
+                    <rect width="24" height="24" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END PARENTS BLOCK -->
   </div>
 
   <?php include "templates/footer.php"; ?>
+  <script>
+  /* -----------------------------------------------------------
+                                                                                                        Effective slider
+                                                                                                  --    --------------------------------------------------------- */
+  $(function() {
+    // PC
+    var parentsFeedBackList = $('.parents-feed-back-list').slick({
+      speed: 600,
+      autoplaySpeed: 6000,
+      autoplay: true,
+      infinite: true,
+      swipe: true,
+      fade: false,
+      dots: false,
+      arrows: false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      initialSlide: 1,
+      responsive: [{
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+    $('.parents-feed-back-list-wrapper .btn-next').on('click', function() {
+      parentsFeedBackList.slick('next');
+    });
+    parentsFeedBackList.slick('slickGoTo', 0)
+  });
+  </script>
 
 </body>
 
