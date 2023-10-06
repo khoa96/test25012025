a94b882b107f1fcb871f6100f44650bfd9c67b18
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-10-06 18:06:23 +0700

    fix: kids

--- kids.php
+++ kids.php
@@ -6,7 +6,10 @@
     <!-- START BANNER BLOCK -->
     <div class="banner-block-wrapper">
       <div class="container">
-        <div class="banner-block-content-wrapper">NGUYEN DANG KHOA</div>
+        <div class="banner-block-content-wrapper">
+          <h1 class="banner-kids-title">Khóa Luyện Nói 1 Kèm 1
+            Với Giáo Viên Người Nước Ngoài Cho Bé</h1>
+        </div>
       </div>
     </div>
     <!-- END BANNER BLOCK -->
@@ -14,23 +17,23 @@
   <?php include "templates/footer.php"; ?>
 
   <script>
-    /* -----------------------------------------------------------
+  /* -----------------------------------------------------------
           Effective slider
         ----------------------------------------------------------- */
-    $(function () {
-      // PC
-      var effectivePCSlider = $('.con-effective .box-slider-pc .box-slider').slick({
-        speed: 600,
-        autoplaySpeed: 6000,
-        autoplay: true,
-        infinite: true,
-        swipe: true,
-        fade: false,
-        dots: false,
-        arrows: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [{
+  $(function() {
+    // PC
+    var effectivePCSlider = $('.con-effective .box-slider-pc .box-slider').slick({
+      speed: 600,
+      autoplaySpeed: 6000,
+      autoplay: true,
+      infinite: true,
+      swipe: true,
+      fade: false,
+      dots: false,
+      arrows: false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [{
           breakpoint: 992,
           settings: {
             slidesToShow: 2
@@ -42,83 +45,83 @@
             slidesToShow: 1
           }
         }
-        ]
-      });
-
-      $('.con-effective .slick-prev').on('click', function () {
-        effectivePCSlider.slick('prev');
-      });
-      $('.con-effective .slick-next').on('click', function () {
-        effectivePCSlider.slick('next');
-      });
-
-      // Mobile
-      $('.con-effective .box-news-mobile .box-slider').slick({
-        speed: 600,
-        autoplaySpeed: 6000,
-        autoplay: true,
-        infinite: true,
-        swipe: true,
-        fade: false,
-        dots: false,
-        arrows: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      });
-
+      ]
     });
 
-    /* -----------------------------------------------------------
-      Youtube modal
-    ----------------------------------------------------------- */
-    $(function () {
-      $(document.body).on('click', '[data-modal="#youtubeModal"]', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
+    $('.con-effective .slick-prev').on('click', function() {
+      effectivePCSlider.slick('prev');
+    });
+    $('.con-effective .slick-next').on('click', function() {
+      effectivePCSlider.slick('next');
+    });
 
-        var modal = $(this).data('modal');
-        var iframe = $(this).data('iframe');
+    // Mobile
+    $('.con-effective .box-news-mobile .box-slider').slick({
+      speed: 600,
+      autoplaySpeed: 6000,
+      autoplay: true,
+      infinite: true,
+      swipe: true,
+      fade: false,
+      dots: false,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
 
-        // Reset
-        $(modal).find('.box-iframe').html('');
+  });
 
-        $.magnificPopup.open({
-          items: {
-            src: modal
+  /* -----------------------------------------------------------
+    Youtube modal
+  ----------------------------------------------------------- */
+  $(function() {
+    $(document.body).on('click', '[data-modal="#youtubeModal"]', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      var modal = $(this).data('modal');
+      var iframe = $(this).data('iframe');
+
+      // Reset
+      $(modal).find('.box-iframe').html('');
+
+      $.magnificPopup.open({
+        items: {
+          src: modal
+        },
+        type: 'inline',
+        mainClass: 'mfp-fade',
+        removalDelay: 200,
+        closeBtnInside: false,
+        callbacks: {
+          open: function() {
+            $(modal).find('.box-iframe').html(iframe);
           },
-          type: 'inline',
-          mainClass: 'mfp-fade',
-          removalDelay: 200,
-          closeBtnInside: false,
-          callbacks: {
-            open: function () {
-              $(modal).find('.box-iframe').html(iframe);
-            },
-            close: function () {
-              $(modal).find('.box-iframe').html('');
-            }
+          close: function() {
+            $(modal).find('.box-iframe').html('');
           }
-        });
+        }
       });
     });
+  });
 
-    /* -----------------------------------------------------------
-      Moderators slider
-    ----------------------------------------------------------- */
-    $(function () {
-      // PC
-      var moderatorsPCSlider = $('.con-moderators .box-slider-pc .box-slider').slick({
-        speed: 600,
-        autoplaySpeed: 6000,
-        autoplay: true,
-        infinite: true,
-        swipe: true,
-        fade: false,
-        dots: false,
-        arrows: false,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [{
+  /* -----------------------------------------------------------
+    Moderators slider
+  ----------------------------------------------------------- */
+  $(function() {
+    // PC
+    var moderatorsPCSlider = $('.con-moderators .box-slider-pc .box-slider').slick({
+      speed: 600,
+      autoplaySpeed: 6000,
+      autoplay: true,
+      infinite: true,
+      swipe: true,
+      fade: false,
+      dots: false,
+      arrows: false,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [{
           breakpoint: 1400,
           settings: {
             slidesToShow: 3
@@ -136,45 +139,45 @@
             slidesToShow: 1
           }
         }
-        ]
-      });
-
-      $('.con-moderators .slick-next').on('click', function () {
-        moderatorsPCSlider.slick('next');
-      });
-
-      // Mobile
-      $('.con-moderators .box-news-mobile .box-slider').slick({
-        speed: 600,
-        autoplaySpeed: 6000,
-        autoplay: true,
-        infinite: true,
-        swipe: true,
-        fade: false,
-        dots: false,
-        arrows: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      });
+      ]
     });
 
-    /* -----------------------------------------------------------
-    Newsbitu slider
-    ----------------------------------------------------------- */
-    $(function () {
-      $('.con-newsbitu .box-news-mobile .box-slider').slick({
-        speed: 600,
-        autoplaySpeed: 6000,
-        autoplay: true,
-        infinite: true,
-        swipe: true,
-        fade: false,
-        dots: false,
-        arrows: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      });
+    $('.con-moderators .slick-next').on('click', function() {
+      moderatorsPCSlider.slick('next');
     });
+
+    // Mobile
+    $('.con-moderators .box-news-mobile .box-slider').slick({
+      speed: 600,
+      autoplaySpeed: 6000,
+      autoplay: true,
+      infinite: true,
+      swipe: true,
+      fade: false,
+      dots: false,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
+
+  /* -----------------------------------------------------------
+  Newsbitu slider
+  ----------------------------------------------------------- */
+  $(function() {
+    $('.con-newsbitu .box-news-mobile .box-slider').slick({
+      speed: 600,
+      autoplaySpeed: 6000,
+      autoplay: true,
+      infinite: true,
+      swipe: true,
+      fade: false,
+      dots: false,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
   </script>
 </body>
 
