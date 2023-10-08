bf5c15dd96730a73c62c764bb7a0d00b84c52595
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-10-08 19:32:05 +0700

    fix kids

--- kids.php
+++ kids.php
@@ -7,177 +7,355 @@
     <div class="banner-block-wrapper">
       <div class="container">
         <div class="banner-block-content-wrapper">
-          <h1 class="banner-kids-title">Khóa Luyện Nói 1 Kèm 1
-            Với Giáo Viên Người Nước Ngoài Cho Bé</h1>
+          <h1 class="banner-kids-title title-for-pc">Khóa Luyện Nói 1 Kèm 1 <br /> Với Giáo Viên Người Nước Ngoài Cho Bé
+          </h1>
+          <h1 class="banner-kids-title title-for-mb">Khóa Luyện Nói <br />1 Kèm 1 <br /> Với Giáo Viên Người <br /> Nước
+            Ngoài
+            Cho Bé
+          </h1>
+
+          <div class="banner-content">
+            <div class="left-content">
+              <div class="image-background">
+                <img src="./images/girl-image.png" class="kid-image kid-image-for-pc" alt=" kid-image-pc" />
+                <img src="./images/girl-image-mb.png" class="kid-image kid-image-for-mb" alt=" kid-image-mb" />
+              </div>
+              <div class="content">
+                <ul class="list-option">
+                  <li class="option-item">
+                    <img src="./images/icon-tick.png" class="icon-tick" alt="icon-tick" />
+                    <p class="option-text">
+                      <span>Tiết kiệm tối đa chỉ</span>
+                      <span class="hight-light">83.000đ/buổi</span>
+                    </p>
+                  </li>
+
+                  <li class="option-item">
+                    <img src="./images/icon-tick.png" class="icon-tick" alt="icon-tick" />
+                    <p class="option-text">
+                      Học online, ba mẹ không cần đưa đón
+                    </p>
+                  </li>
+
+                  <li class="option-item">
+                    <img src="./images/icon-tick.png" class="icon-tick" alt="icon-tick" />
+                    <p class="option-text">
+                      Cam kết chuẩn đầu ra Cambridge
+                    </p>
+                  </li>
+                  <li class="option-item">
+                    <img src="./images/icon-tick.png" class="icon-tick" alt="icon-tick" />
+                    <p class="option-text">
+                      Nhận chứng chỉ hoàn thành khóa học
+                    </p>
+                  </li>
+
+                  <li class="option-item">
+                    <img src="./images/icon-tick.png" class="icon-tick" alt="icon-tick" />
+                    <p class="option-text">
+                      <span>Toàn bộ giáo viên đều có</span>
+                      <span class="hight-light">chứng chỉ giảng dạy quốc tế</span>
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="right-content">
+              <ul class="count-down-timer" id="coun-down-timer">
+                <li class="timer-item">
+                  <span class="number">00</span>
+                  <span class="label">Ngày</span>
+                </li>
+                <li class="timer-item">
+                  <span class="number">05</span>
+                  <span class="label">Giờ</span>
+                </li>
+                <li class="timer-item">
+                  <span class="number">34</span>
+                  <span class="label">Phút</span>
+                </li>
+                <li class="timer-item">
+                  <span class="number">50</span>
+                  <span class="label">Giây</span>
+                </li>
+              </ul>
+              <h2 class="register-title">Mua khóa học ngay để nhận ưu đãi</h2>
+              <div class="register-form">
+                <form method="" action="">
+                  <input type="text" name="" class="input-form" value="" placeholder="Họ tên" />
+                  <input type="text" name="" class="input-form" value="" placeholder="Số điện thoại" />
+                  <input type="text" name="" class="input-form" value="" placeholder="Tuổi con" />
+                  <button type="" class="btn-register">ĐĂNG KÝ HỌC THỬ</button>
+                </form>
+              </div>
+              <div class="bottom-image">
+                <img src="./images/bottom-form-image.png" class="bottom-form-image" alt="">
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <!-- END BANNER BLOCK -->
-  </div>
-  <?php include "templates/footer.php"; ?>
 
+    <!-- START BITU FOR BLOCK -->
+    <div class="bitu-for-block-wrapper">
+      <div class="bitu-for-content-wrapper">
+        <div class="container center">
+          <div class="common-title-with-background  bitu-for-title">
+            <h1 class="title-text txt-uppercase">khoá học bitu dành cho</h1>
+          </div>
+          <div class=" bitu-for-main">
+            <div class='list-option'>
+              <img src="./images/bitu-for-1.png" class="option-item" alt="" />
+              <img src="./images/bitu-for-2.png" class="option-item" alt="" />
+              <img src="./images/bitu-for-3.png" class="option-item" alt="" />
+              <img src="./images/bitu-for-4.png" class="option-item" alt="" />
+            </div>
+            <div class="main-image-wrapper">
+              <img src="./images/bitu-for-girl.png" class="girl-image " alt="">
+            </div>
+          </div>
+          <div class="bottom-icon-wrapper">
+            <img src="./images/icon-bitu-1.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END BITU FOR BLOCK -->
+
+    <!-- START LEARN ENGLISH WITH FOREIGNER TEACHER BLOCK -->
+    <div class="learn-with-foreigner-teacher-wrapper">
+      <div class="container">
+        <div class="learn-with-foreigner-teacher-content">
+          <div class="title-block">
+            <div class="common-title-with-background title-1 learn-with-foreigner-teacher-title">
+              <h1 class="title-text txt-uppercase">học 1 kèm 1 với</h1>
+            </div>
+            <img src="./images/icon-rocket.png" class="icon-rocket" alt="">
+            <div class="common-title-with-background title-2 learn-with-foreigner-teacher-title">
+              <h1 class="title-text txt-uppercase">giáo viên nước ngoài</h1>
+            </div>
+          </div>
+
+          <div class="list-feature-of-class">
+            <div class="left-content">
+              <div class="video-wrapper">
+                <div>video content</div>
+              </div>
+            </div>
+            <div class="right-content">
+              <ul class="list-option-of-class">
+                <li class="option-of-class-item">
+                  <img src="./images/icon-tick-2.png" class="icon-tick" alt="">
+                  <p class="text">Lớp học chỉ có 1 giáo viên nước ngoài và 1 bé.</p>
+                </li>
+                <li class="option-of-class-item">
+                  <img src="./images/icon-tick-2.png" class="icon-tick" alt="">
+                  <p class="text">Con được chỉ lỗi sai khi
+                    phát âm, dùng từ và được
+                    hướng dẫn lại cho đúng.</p>
+                </li>
+                <li class="option-of-class-item">
+                  <img src="./images/icon-tick-2.png" class="icon-tick" alt="">
+                  <p class="text">Học cùng 100% giáo viên
+                    nước ngoài, hình thành
+                    phản xạ giao tiếp tự nhiên.</p>
+                </li>
+
+              </ul>
+            </div>
+          </div>
+          <div class="register-block">
+            <button type="" class="btn-register">ĐĂNG KÝ HỌC THỬ</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END LEARN ENGLISH WITH FOREIGNER TEACHER BLOCK -->
+
+    <!--   START LEARN ENGLISH WITH FOREIGNER TEACHER BLOCK 2-->
+    <div class="list-kid-student-wrapper">
+      <div class="container">
+        <div class="title-block-wrapper">
+          <div class="common-title-with-background title-1">
+            <h1 class="title-text txt-uppercase">Các bé học 1 kèm 1</h1>
+          </div>
+          <div class="common-title-with-background title-2">
+            <h1 class="title-text txt-uppercase">cùng giáo viên nước ngoài tại Bitu</h1>
+          </div>
+        </div>
+
+        <div class="list-class-of-kid-wrapper">
+          <div class="list-class-of-student">
+            <div class="class-item">
+              <div class="video-item">1</div>
+            </div>
+            <div class="class-item">
+              <div class="video-item">2</div>
+            </div>
+            <div class="class-item">
+              <div class="video-item">3</div>
+            </div>
+          </div>
+          <button class="btn-slider btn-next">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <g clip-path="url(#clip0_5877_8806)">
+                <path d="M9 6L15 12L9 18" stroke="#1A202C" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round" />
+              </g>
+              <defs>
+                <clipPath id="clip0_5877_8806">
+                  <rect width="24" height="24" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </button>
+
+        </div>
+      </div>
+    </div>
+    <!-- END LEARN ENGLISH WITH FOREIGNER TEACHER BLOCK 2  -->
+
+    <!-- BITU RECOMMEND USING BLOCK -->
+    <div class="bitu-recommend-wrapper">
+      <div class="container">
+        <div class="title-block-wrapper">
+          <div class="common-title-with-background recommend-title">
+            <h1 class="title-text txt-uppercase">Bitu được VTV khuyên dùng</h1>
+          </div>
+        </div>
+
+        <div class="bitu-recommend-list-wrapper">
+          <div class="bitu-recommend-list">
+            <div class="class-item">
+              <div class="video-item">1</div>
+            </div>
+            <div class="class-item">
+              <div class="video-item">2</div>
+            </div>
+          </div>
+          <button class="btn-slider btn-next">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <g clip-path="url(#clip0_5877_8806)">
+                <path d="M9 6L15 12L9 18" stroke="#1A202C" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round" />
+              </g>
+              <defs>
+                <clipPath id="clip0_5877_8806">
+                  <rect width="24" height="24" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </button>
+
+        </div>
+      </div>
+    </div>
+    <!-- END RECOMMEND USING BLOCK -->
+  </div>
+
+
+  <?php include "templates/footer.php"; ?>
   <script>
-  /* -----------------------------------------------------------
-          Effective slider
-        ----------------------------------------------------------- */
-  $(function() {
-    // PC
-    var effectivePCSlider = $('.con-effective .box-slider-pc .box-slider').slick({
-      speed: 600,
-      autoplaySpeed: 6000,
-      autoplay: true,
-      infinite: true,
-      swipe: true,
-      fade: false,
-      dots: false,
-      arrows: false,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [{
+    $(function () {
+      // PC
+      var nativeTeacherSlider = $('.list-class-of-student').slick({
+        speed: 600,
+        autoplaySpeed: 6000,
+        autoplay: false,
+        infinite: false,
+        swipe: false,
+        fade: false,
+        dots: false,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
           breakpoint: 992,
           settings: {
-            slidesToShow: 2
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 600,
+            autoplaySpeed: 6000,
+            autoplay: true,
+            infinite: true,
+            swipe: true,
+            fade: false,
+            dots: false,
+            arrows: false,
           }
         },
         {
           breakpoint: 768,
           settings: {
-            slidesToShow: 1
+            slidesToShow: 1,
+            speed: 600,
+            autoplaySpeed: 6000,
+            slidesToScroll: 1,
+            autoplay: false,
+            infinite: true,
+            swipe: true,
+            fade: false,
+            dots: false,
+            arrows: false,
           }
         }
-      ]
-    });
+        ]
+      });
 
-    $('.con-effective .slick-prev').on('click', function() {
-      effectivePCSlider.slick('prev');
-    });
-    $('.con-effective .slick-next').on('click', function() {
-      effectivePCSlider.slick('next');
-    });
+      $('.list-class-of-kid-wrapper .btn-next').on('click', function () {
+        nativeTeacherSlider.slick('next');
+      });
 
-    // Mobile
-    $('.con-effective .box-news-mobile .box-slider').slick({
-      speed: 600,
-      autoplaySpeed: 6000,
-      autoplay: true,
-      infinite: true,
-      swipe: true,
-      fade: false,
-      dots: false,
-      arrows: false,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    });
-
-  });
-
-  /* -----------------------------------------------------------
-    Youtube modal
-  ----------------------------------------------------------- */
-  $(function() {
-    $(document.body).on('click', '[data-modal="#youtubeModal"]', function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-
-      var modal = $(this).data('modal');
-      var iframe = $(this).data('iframe');
-
-      // Reset
-      $(modal).find('.box-iframe').html('');
-
-      $.magnificPopup.open({
-        items: {
-          src: modal
+      var bituRecommend = $('.bitu-recommend-list').slick({
+        speed: 600,
+        autoplaySpeed: 6000,
+        autoplay: false,
+        infinite: false,
+        swipe: false,
+        fade: false,
+        dots: false,
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [{
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 600,
+            autoplaySpeed: 6000,
+            autoplay: true,
+            infinite: true,
+            swipe: true,
+            fade: false,
+            dots: false,
+            arrows: false,
+          }
         },
-        type: 'inline',
-        mainClass: 'mfp-fade',
-        removalDelay: 200,
-        closeBtnInside: false,
-        callbacks: {
-          open: function() {
-            $(modal).find('.box-iframe').html(iframe);
-          },
-          close: function() {
-            $(modal).find('.box-iframe').html('');
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            speed: 600,
+            autoplaySpeed: 6000,
+            slidesToScroll: 1,
+            autoplay: false,
+            infinite: true,
+            swipe: true,
+            fade: false,
+            dots: false,
+            arrows: false,
           }
         }
+        ]
+      });
+
+      $('.bitu-recommend-list-wrapper .btn-next').on('click', function () {
+        bituRecommend.slick('next');
       });
     });
-  });
-
-  /* -----------------------------------------------------------
-    Moderators slider
-  ----------------------------------------------------------- */
-  $(function() {
-    // PC
-    var moderatorsPCSlider = $('.con-moderators .box-slider-pc .box-slider').slick({
-      speed: 600,
-      autoplaySpeed: 6000,
-      autoplay: true,
-      infinite: true,
-      swipe: true,
-      fade: false,
-      dots: false,
-      arrows: false,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      responsive: [{
-          breakpoint: 1400,
-          settings: {
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 2
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1
-          }
-        }
-      ]
-    });
-
-    $('.con-moderators .slick-next').on('click', function() {
-      moderatorsPCSlider.slick('next');
-    });
-
-    // Mobile
-    $('.con-moderators .box-news-mobile .box-slider').slick({
-      speed: 600,
-      autoplaySpeed: 6000,
-      autoplay: true,
-      infinite: true,
-      swipe: true,
-      fade: false,
-      dots: false,
-      arrows: false,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    });
-  });
-
-  /* -----------------------------------------------------------
-  Newsbitu slider
-  ----------------------------------------------------------- */
-  $(function() {
-    $('.con-newsbitu .box-news-mobile .box-slider').slick({
-      speed: 600,
-      autoplaySpeed: 6000,
-      autoplay: true,
-      infinite: true,
-      swipe: true,
-      fade: false,
-      dots: false,
-      arrows: false,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    });
-  });
   </script>
 </body>
 
---
+++ test.php
@@ -0,0 +1,11 @@
+<div class="common-title-with-background  bitu-for-title">
+  <h1 class="title-text txt-uppercase">khoá học bitu dành cho</h1>
+</div>
+
+<div class="common-title-with-background title-1 learn-with-foreigner-teacher-title">
+  <h1 class="title-text txt-uppercase">học 1 kèm 1 với</h1>
+</div>
+<img src="./images/icon-rocket.png" class="icon-rocket" alt="">
+<div class="common-title-with-background title-2 learn-with-foreigner-teacher-title">
+  <h1 class="title-text txt-uppercase">giáo viên nước ngoài</h1>
+</div>
